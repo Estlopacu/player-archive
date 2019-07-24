bef28bb7d6b25ce38233890c748a03fc0589e793
Estlopacu <esteban.lopez@getyourguide.com>
2019-07-25 00:46:16 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { PlayersListComponent } from './players-list/players-list.component';
+import { PlayerComponent } from './player/player.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchFormComponent,
+    PlayersListComponent,
+    PlayerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
